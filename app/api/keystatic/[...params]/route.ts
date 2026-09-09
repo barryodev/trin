b64d6e289bb0276eb9630a23471adc0baf855b64
barryodev <barryodev@gmail.com>
2026-09-09 08:41:42 +0100

    Fix Keystatic GitHub OAuth scope

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,6 +1,29 @@
 import { makeRouteHandler } from "@keystatic/next/route-handler";
 import keystaticConfig from "@/keystatic.config";
 
-export const { GET, POST } = makeRouteHandler({
+const keystaticRouteHandler = makeRouteHandler({
   config: keystaticConfig,
 });
+
+export const POST = keystaticRouteHandler.POST;
+
+export async function GET(request: Request) {
+  const response = await keystaticRouteHandler.GET(request);
+  const location = response.headers.get("location");
+
+  if (location?.startsWith("https://github.com/login/oauth/authorize")) {
+    const authorizationUrl = new URL(location);
+    authorizationUrl.searchParams.set("scope", "public_repo");
+
+    const headers = new Headers(response.headers);
+    headers.set("location", authorizationUrl.toString());
+
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers,
+    });
+  }
+
+  return response;
+}
